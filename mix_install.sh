935df6708d8ffba6f7fd680d2def7c31f8a7e631
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-10-28 12:16:30 +0800

    Update mix_install.sh

--- mix_install.sh
+++ mix_install.sh
@@ -3,8 +3,8 @@
 #开始
 echo "给即将安装的软件设置一个文件夹名称"
 read -p "请输入英文字母:" yourdir
-dir = "/usr/src/"$yourdir
-if [! -d $dir]; then
+ifdir = "/usr/src/"${yourdir}
+if [! -d $ifdir]; then
 #下载几个配置文件
 mkdir /usr/src/$yourdir
 mkdir /usr/src/$yourdir/client
